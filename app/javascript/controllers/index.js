fb89bae0fe10c9083a473248f61c5ea0e95560c6
Mariana Gimenez <gimenezmarianae@gmail.com>
2023-03-09 17:44:33 +0100

    index plant page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SearchPlantController from "./search_plant_controller"
+application.register("search-plant", SearchPlantController)
