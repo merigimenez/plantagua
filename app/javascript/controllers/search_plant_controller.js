82d1e23d2d256924100dd875e80f8eea594bc161
Mariana Gimenez <gimenezmarianae@gmail.com>
2023-03-10 16:24:09 +0100

    plant show, correct plant index search

--- app/javascript/controllers/search_plant_controller.js
+++ app/javascript/controllers/search_plant_controller.js
@@ -3,12 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-plant"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
+  static values = { garden: Number }
 
   connect() {
   }
 
   update() {
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}&garden_id=${this.gardenValue}`
     fetch(url, {headers: {"accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
