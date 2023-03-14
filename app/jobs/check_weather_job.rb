72f1549e4993da2917b0bbd20eba94001dcbbc3c
Mariana Gimenez <gimenezmarianae@gmail.com>
2023-03-14 16:52:23 +0100

    fix in sidekiq

--- app/jobs/check_weather_job.rb
+++ app/jobs/check_weather_job.rb
@@ -4,8 +4,10 @@ class CheckWeatherJob < ApplicationJob
   def perform(garden)
     rain = WeatherService.new(garden).call
     garden_plants = garden.garden_plants
+    return unless rain
+
     garden_plants.each do |garden_plant|
-      garden_plant.update(last_day: Date.today) if garden_plant.outdoor && rain
+      garden_plant.update(last_day: Date.today) if garden_plant.outdoor
     end
   end
 end
