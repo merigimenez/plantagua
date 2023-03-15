a12111e4f3ca6960f26dbb23f155f1ab1cc95a6b
Genevieve Warthon Landmann <genevievewarthon@gmail.com>
2023-03-15 20:18:05 +0100

    front bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,11 +26,11 @@ class ApplicationController < ActionController::Base
   # end
 
   def after_sign_in_path_for(resource)
-    gardens_path(current_user)
+    gardens_path
   end
 
   def after_sign_up_path_for(resource)
-    gardens_path(current_user)
+    gardens_path
   end
 
   def default_url_options
